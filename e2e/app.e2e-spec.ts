f3ced748bd1e16603a8c98b63b957d35d40f63cf
Bruno João <bruno@tzadi.com>
2017-10-15 15:49:20 -0200

    Angular project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('vinagre App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
