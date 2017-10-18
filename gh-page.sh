bcff521728b4c0b703adbfbb9899b38d76c4964b
Bruno João <bruno@tzadi.com>
2017-10-18 00:04:49 -0200

    fix gh-page

--- gh-page.sh
+++ gh-page.sh
@@ -5,6 +5,7 @@ git add -u
 git commit -m "Build GH PAGES - To revert this, squash the last commit."
 ng build --prod --output-path="./../dist-foeco.temp" --aot --base-href="https://vinagreti.github.io/"
 cp ./404.html ./../dist-foeco.temp/
+cp ./gh-page.sh ./../dist-foeco.temp/
 
 git checkout master
 find . -type f -not -nem='.gitignore' -print0 | xargs -0 rm --
@@ -12,7 +13,7 @@ find . -type f -not -nem='.gitignore' -print0 | xargs -0 rm --
 mv ./../dist-foeco.temp/* ./
 git add --all
 git add -u
-git commit -m "Buil master from master.sh"
+git commit -m "Buil production from master.sh"
 git push --force
 rm -r ./../dist-foeco.temp
 git checkout develop
