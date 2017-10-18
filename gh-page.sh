90fa7b83b34276158b2424b45a163278a44c133f
Bruno João <bruno@tzadi.com>
2017-10-17 23:46:30 -0200

    Build GH PAGES - To revert this, squash the last commit.

--- gh-page.sh
+++ gh-page.sh
@@ -6,13 +6,13 @@ git commit -m "Build GH PAGES - To revert this, squash the last commit."
 ng build --prod --output-path="./../dist-foeco.temp" --aot --base-href="https://vinagreti.github.io/"
 cp ./404.html ./../dist-foeco.temp/
 
-git checkout gh-pages
+git checkout develop
 find . -type f -not -nem='.gitignore' -print0 | xargs -0 rm --
 
 mv ./../dist-foeco.temp/* ./
 git add --all
 git add -u
-git commit -m "Buil production from gh-pages.sh"
+git commit -m "Buil master from develop.sh"
 git push --force
 rm -r ./../dist-foeco.temp
-git checkout master
+git checkout develop
