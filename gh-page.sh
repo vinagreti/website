f2ce41eb007d801a7ae6b5d32e8df70d180d6d6e
Bruno João <bruno@tzadi.com>
2017-10-26 20:27:25 -0200

    Saving/Commiting current branch

--- gh-page.sh
+++ gh-page.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-git add --all &&
-git add -u &&
-git commit -m "Saving/Commiting current branch" &&
-ng build --prod --aot --base-href="https://vinagreti.github.io/" &&
-cp ./index.html ./dist/404.html &&
+git add --all
+git add -u
+git commit -m "Saving/Commiting current branch"
+ng build --prod --aot --base-href="https://vinagreti.github.io/"
+cp ./index.html ./dist/404.html
 mv dist gh-page
-git checkout $1 &&
-ls | grep -v gh-page | xargs rm -r &&
-git add --all &&
-git commit -m "Buil $1 from ${git branch}" &&
-git push --force &&
+git checkout $1
+ls | grep -v gh-page | xargs rm -r
+git add --all
+git commit -m "Buil $1 from ${git branch}"
+git push --force
 git checkout develop
