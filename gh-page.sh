f342cca274d2e2a7490f71c07bfa5e5494699edc
Bruno João <bruno@tzadi.com>
2017-10-26 22:47:38 -0200

    s

--- gh-page.sh
+++ gh-page.sh
@@ -1,13 +1,30 @@
-#!/bin/bash
+@tz #!/bin/bash
 
-git add --all &&
-git add -u &&
-git commit -m "Saving/Commiting current branch" ;
-ng build --prod --base-href="https://vinagreti.github.io/" &&
-cp ./index.html ./dist/404.html &&
-mv dist gh-page &&
-git checkout $1 &&
-git add --all &&
-git commit -m "Buil $1 from ${git branch}" &&
-git push --force &&
-git checkout develop
+if [[ $1 && $2 ]]; then
+  if [[ $2 === 'ngjhg98745345igASfg34gDFGs3' ]]; then
+    BRANCH="$(git branch -v)"
+    rm -r gh-page ;
+    git add . && \
+    git add -u && \
+    git commit -m "Saving/Commiting current branch" ;
+    ng build --prod --base-href="https://vinagreti.github.io/" && \
+    cp ./index.html ./dist/404.html && \
+    mv dist/ gh-page && \
+    git checkout $1 && \
+    ls | grep -v gh-page | grep -v node_modules | grep -v tsconfig.app.json | xargs rm -r && \
+    mv gh-page/* ./ && \
+    git add . && \
+    git commit -m "GH-PAGE Build ${BRANCH} into $1" && \
+    git push --force && \
+    git checkout develop &&
+    rm -r gh-page &&
+    cp gh-page.sh ../.gh-page.sh
+  else
+      echo 'OOOOOPS... Second parameter is not valid'
+  fi
+else if [[ $1 ]]; then
+  cp gh-page.sh ../.gh-page.sh &&
+  ./../.gh-page.sh $1 'ngjhg98745345igASfg34gDFGs3'
+else
+  echo 'OOOOOPS... Inform the target branch :D'
+fi
