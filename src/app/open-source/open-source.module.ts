4093b34712d46f616b9e2b40f0e6e4a776c79603
Bruno João <bruno@tzadi.com>
2017-11-18 11:00:29 -0200

    v1.0.0 - MVP

--- src/app/open-source/open-source.module.ts
+++ src/app/open-source/open-source.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatCardModule } from '@angular/material';
 import { OpenSourceComponent } from './open-source.component';
 import { OpenSourceRoutingModule } from './open-source-routing.module';
+import { AdminOnlyDirectiveModule } from './../shared/directives/admin-only/admin-only.module';
 
 @NgModule({
   imports: [
+    AdminOnlyDirectiveModule,
     CommonModule,
     MatButtonModule,
     MatCardModule,
